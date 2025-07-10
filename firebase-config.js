07ae2613695ffa05d748f25941a3c72a2c372357
lordrahl2-sys <s65848383@gmail.com>
2025-07-10 11:15:18 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
-
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAkDLjFGnGudVZXqMmvTAxs72mwDD3hDks",
   authDomain: "rahl-host.firebaseapp.com",
@@ -11,18 +6,4 @@ const firebaseConfig = {
   messagingSenderId: "98638028534",
   appId: "1:98638028534:web:1f9ad66a58bd1af52d56d7",
   measurementId: "G-5SD4095M7R"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const storage = getStorage(app);
-
-export {
-  auth,
-  storage,
-  ref,
-  uploadBytes,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
 };
